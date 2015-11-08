0ef234b491eecb32cb46c91725f17648eab0d2be
Michael Harrison <mwharrison@rice.edu>
2015-11-07 23:55:01 -0600

    Jessicas time app!

--- Tell Me The Time/ViewController.swift
+++ Tell Me The Time/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var crazyImahe: UIImageView!
+    
+    let name = "Jessica"
+    
+    @IBAction func timeButton(sender: AnyObject) {
+    
+        let date = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.timeStyle = .ShortStyle
+        let string = formatter.stringFromDate(date)
+        let timeString =  "\(name) it is \(string) bitch!"
+        timeLabel.text = timeString
+        crazyImahe.hidden = false
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        crazyImahe.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
